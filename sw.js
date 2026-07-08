f1106ee57502ea47cb7721eacd9b5910b99bf98b
Sina Alp <sinanalp356@gmail.com>
2026-07-08 02:15:56 +0000

    Deploy: güncel build (kökten düzeltme)

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'dostos-cache-v' + new Date().getTime();
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match(event.request))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
